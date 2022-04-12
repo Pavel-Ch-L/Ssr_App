012b285cc8ce7746b397f89c3c32df6f297624a2
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-04-12 20:41:47 +0600

    Ch_3-32 Model card (bucket)

--- Ch_3/Models/card.js
+++ Ch_3/Models/card.js
@@ -1,5 +1,49 @@
-class Card {
+const path = require('path')
+const fs = require('fs')
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'card.json'
+)
 
+class Card {
+  static async add(course) {
+    const card = await Card.fetch()
+
+    const idx = card.courses.findIndex(c => c.id === course.id)
+    const candidate = card.courses[idx]
+    if(candidate) {
+      //курс уже есть
+      candidate.count++
+      card.courses[idx] = candidate
+    } else {
+      //нужно добавить
+      course.count = 1
+      card.courses.push(course)
+    }
+    card.price += +course.price
+    return new Promise((res, rej) => {
+      fs.writeFile(p, JSON.stringify(card), err => {
+        if(err) {
+          rej(err)
+        } else {
+          res()
+        }
+      })
+    })
+  }
+
+  static async fetch() {
+    return new Promise((res, rej) => {
+      fs.readFile(p, 'utf-8', (err, content) => {
+        if(err){
+          rej(err)
+        } else {
+          res(JSON.parse(content))
+        }
+      })
+    })
+  }
 }
 
 module.exports = Card
