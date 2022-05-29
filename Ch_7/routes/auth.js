81edde550a4d05e8a73a9db4eae878ce6d2a4424
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-05-29 16:28:59 +0600

    Ch_7-73 Async validators

--- Ch_7/routes/auth.js
+++ Ch_7/routes/auth.js
@@ -55,17 +55,12 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', registerValidators, async (req, res) => {
   try {
-    const {email, password, confirm, name} = req.body
-    const candidate = await User.findOne({email})
+    const {email, password, name} = req.body
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       req.flash('registerError', errors.array()[0].msg)
       return res.status(422).redirect('/auth/login#register')
     }
-    if (candidate) {
-      req.flash('registerError', 'Такой email уже занят')
-      res.redirect('/auth/login#register')
-    } else {
       const hashPassword = await bcrypt.hash(password, 10)
       const user = new User({
         email, name, password: hashPassword, cart:{items: []}
@@ -73,7 +68,6 @@ router.post('/register', registerValidators, async (req, res) => {
       await user.save()
       res.redirect('/auth/login#login')
       await mailTransport(regEmail(email, name))
-    }
   } catch (e) {
     console.log(e);
   }
--- Ch_7/utils/validators.js
+++ Ch_7/utils/validators.js
@@ -1,8 +1,18 @@
 const {body} = require('express-validator')
+const User = require('../models/user')
 
 
 exports.registerValidators = [
-  body('email').isEmail().withMessage('Введите корректный Email'),
+  body('email').isEmail().withMessage('Введите корректный Email').custom( async (value, {req} ) => {
+    try {
+      const user = await User.findOne({email: value})
+      if(user) {
+        return Promise.reject('Такой email уже занят')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }),
   body('password', 'Пароль должен быть минимум 6 символов').isLength({min: 6, max: 56}).isAlphanumeric(),
   body('confirm').custom( (value, {req}) => {
     if(value !== req.body.password) {
