1c91176c8940ecaad047fb50f063e73423809918
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-06-05 16:38:41 +0600

    Ch_1-2 init

--- Ch_1-2/Refs/Console.js
+++ Ch_1-2/Refs/Console.js
@@ -1,15 +1,18 @@
-//Параметры консоли
-//console.log(process.argv)
+/* Параметры консоли (аргументы командной строки)
+
+node console a=1 b=2 c=3  // => { a: '1', b: '2', c: '3' }
+
+process.argv  
+
+ */
 
 function consoleJson() {
   const cons = {}
-
   for (let i = 2; i < process.argv.length; i++) {
     const arg = process.argv[i].split('=')
     cons[arg[0]] = arg[1] ? arg[1] : true
   }
-  
   return cons
 }
-
+console.log(process.argv);
 console.log(consoleJson())
